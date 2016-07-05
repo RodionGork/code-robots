41715a216b68cd33c2ec97cd72d010d1108015c1
Rodion Gorkovenko <RodionGork@github.com>
2016-07-05 08:42:10 +0300

    Brython recording execution to window.operations

--- runner.py
+++ runner.py
@@ -1,8 +1,27 @@
+import _sys
 from browser import document, window, alert
 
+init_code = '''
+from browser import window
+
+window.operations = []
+
+'''
+
+def _write(x):
+    window.operations.append(['prn', x])
+
+def _write_err(x):
+    alert(x)
+
+def forward():
+    window.operations.append(['fwd'])
+
 def runner(e):
-    ns = {'__name__':'__main__', 'alert':alert}
-    exec(window.brEditor.getValue(), ns)
+    ns = {'__name__':'__main__', 'alert':alert, 'forward':forward}
+    exec(init_code + window.brEditor.getValue(), ns)
 
 document['mybutton'].bind('click',runner)
 
+_sys.stdout.write = _write
+_sys.stderr.write = _write_err
